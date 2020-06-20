429b4c4a97430b070c01af09663b9161e1b59a7f
Bruno Pádua <brunospadua@gmail.com>
2020-06-20 16:49:58 -0300

    Forms Tag final

--- src/mobx/index.ts
+++ src/mobx/index.ts
@@ -4,6 +4,7 @@ import { router } from './router.store';
 import { starWars } from '../containers/star-wars/stores';
 import { starWarsDetails } from '../containers/star-wars-details/store';
 import { cache } from '../containers/cache/store';
+import { tags } from '../containers/tags/store';
 
 export {
   router,
@@ -11,5 +12,6 @@ export {
   home,
   starWars,
   starWarsDetails,
-  cache
+  cache,
+  tags
 }
